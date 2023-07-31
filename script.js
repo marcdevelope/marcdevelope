689e43bab8bbeea488277222e2358171abf4d495
marcdevelope <140992281+marcdevelope@users.noreply.github.com>
2023-07-31 09:04:22 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,60 @@
+/** */
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
+
+
+/** */
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach (sec =>{
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height) {
+            navLinks.forEach(links =>{
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
+
+    /** */
+    let header = document.querySelector('header');
+
+    header.classList.offsetTop('sticky', window.scrollY > 100);
+
+    /** */
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+};
+
+/** */
+ScrollReveal({ 
+    reset: true, 
+    distance: '80px',
+    duration:2000,
+    delay: 200
+});
+
+ScrollReveal().reveal('.home-content, .heading, .footer, .footer-text p', { origin: 'top' });
+ScrollReveal().reveal('.home-img, .program-box, .program-cointaner, .location-frame', {origin: 'bottom'});
+ScrollReveal().reveal('.home-content h1, .about-img img, .about-content p, .location-content p', {origin: 'left'});
+
+/** */
+const typed = new Typed('.multiple-text',{
+    strings: ['Center', 'Care', 'Platform'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop : true
+});
+
